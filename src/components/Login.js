190f3eaa027311d5d9d4c629717a74cfc84a6c12
jothiboss <124765483+Jothibos@users.noreply.github.com>
2023-09-05 14:08:08 +0530

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,119 @@
- import React, { useState } from "react";
- import { Container, Form, Button } from "react-bootstrap";
- import { useNavigate } from "react-router-dom"; // Import useNavigate
+   import React, { useState } from "react";
+   import { useNavigate, Link } from "react-router-dom";
+   import { Form, Button, Alert } from "react-bootstrap";
 
- const Login = () => {
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   const navigate = useNavigate(); // Get the navigate function from react-router-dom
+   const Login = () => {
+     const navigate = useNavigate();
+     const [errorMessage, setErrorMessage] = useState("");
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
 
-   const handleLogin = (e) => {
-     e.preventDefault();
+     const usersData = JSON.parse(localStorage.getItem("usersData")) || [];
 
-     console.log("Logged in with:", email, password);
+     const handleSignIn = (event) => {
+       event.preventDefault();
 
-     // Redirect to home page
-     navigate("/");
+       // Trim the input values to remove whitespace
+       const trimmedEmail = email.trim();
+       const trimmedPassword = password.trim();
+
+       // Check if the entered credentials match any user's data
+       const validUser = usersData.find(
+         (user) =>
+           user.email === trimmedEmail && user.password === trimmedPassword
+       );
+
+       if (validUser) {
+         alert("Sign In successful!");
+
+         // Simulate the user data after successful login
+
+         // Redirect to the home page after successful sign-in
+         navigate("/");
+       } else {
+         setErrorMessage(" fill out email or password. Please try again.");
+       }
+     };
+
+     const formStyles = {
+       maxWidth: "400px",
+       margin: "0 auto",
+       padding: "20px",
+       border: "1px solid #ccc",
+       borderRadius: "8px",
+       backgroundColor: "#fff",
+       boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+     };
+
+     const labelStyles = {
+       fontWeight: "bold",
+     };
+
+     const inputStyles = {
+       width: "100%",
+       padding: "10px",
+       border: "1px solid #ccc",
+       borderRadius: "4px",
+       backgroundColor: "#f7f7f7",
+       fontSize: "14px",
+     };
+
+     const buttonStyles = {
+       backgroundColor: "#007bff",
+       color: "#fff",
+       border: "none",
+       borderRadius: "4px",
+       padding: "10px 20px",
+       fontSize: "16px",
+       cursor: "pointer",
+     };
+
+     const alertStyles = {
+       marginTop: "10px",
+     };
+
+     return (
+       <div style={formStyles}>
+         <h2 style={{ fontSize: "24px", marginBottom: "20px" }}>Sign In</h2>
+         <Form onSubmit={handleSignIn}>
+           <Form.Group controlId="email">
+             <Form.Label style={labelStyles}>Email ID</Form.Label>
+             <Form.Control
+               style={inputStyles}
+               type="email"
+               name="email"
+               placeholder="Email ID"
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+             />
+           </Form.Group>
+           <Form.Group controlId="password">
+             <Form.Label style={labelStyles}>Password</Form.Label>
+             <Form.Control
+               style={inputStyles}
+               type="password"
+               name="password"
+               placeholder="Password"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+             />
+           </Form.Group>
+           <Button style={buttonStyles} variant="primary" type="submit">
+             Login
+           </Button>
+         </Form>
+         {errorMessage && (
+           <Alert style={alertStyles} variant="danger">
+             {errorMessage}
+           </Alert>
+         )}
+         <div style={{ marginTop: "20px", textAlign: "center" }}>
+           user credentials <Link to="/"> Login</Link>
+           <br />
+           Don't have an account? <Link to="/signup">Sign Up</Link>
+         </div>
+       </div>
+     );
    };
 
-   return (
-     <Container className="centered-form">
-       <h2>Login</h2>
-       <Form onSubmit={handleLogin}>
-         <Form.Group controlId="email">
-           <Form.Label>Email</Form.Label>
-           <Form.Control
-             type="email"
-             placeholder="Enter email"
-             value={email}
-             onChange={(e) => setEmail(e.target.value)}
-             required
-           />
-         </Form.Group>
-
-         <Form.Group controlId="password">
-           <Form.Label>Password</Form.Label>
-           <Form.Control
-             type="password"
-             placeholder="Password"
-             value={password}
-             onChange={(e) => setPassword(e.target.value)}
-             required
-           />
-         </Form.Group>
-
-         <Button variant="primary" type="submit">
-           Login
-         </Button>
-       </Form>
-     </Container>
-   );
- };
-
- export default Login;
+   export default Login;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,37 +1,149 @@
- import React from "react";
- import { Container, Row, Col, Image } from "react-bootstrap";
+   import React, { useState } from "react";
 
- const Profile = () => {
-   // Dummy user data for testing
-   const dummyUser = {
-     firstName: "Jothi boss",
-     lastName: "M",
-     email: "jothibos14@gmail.com",
-     profilePhoto: "/images/profile.jpg",
+   const UserProfile = () => {
+     // Use state to manage user information
+     const [userInfo, setUserInfo] = useState({
+       profilePicture: "****",
+       userName: "*****",
+       email: "********",
+       password: "********",
+       phoneNumber: "**********",
+       address: "*****,*****,****",
+     });
+
+     const [isEditing, setIsEditing] = useState(false);
+
+     const handleInputChange = (field, value) => {
+       setUserInfo((prevInfo) => ({
+         ...prevInfo,
+         [field]: value,
+       }));
+     };
+
+     const handleEditClick = () => {
+       setIsEditing(true);
+     };
+
+     const handleSaveClick = () => {
+       setIsEditing(false);
+       // Here you would typically update the user information using an API call or a data management library
+     };
+
+     const handleDeleteClick = () => {
+       // Here you would typically delete the user using an API call or a data management library
+     };
+
+     return (
+       <div style={styles.container}>
+         <h2 style={styles.heading}>User Profile</h2>
+         <div style={styles.userInfo}>
+           <div>
+             <label style={styles.label}>Profile Picture</label>
+             <img
+               src={userInfo.profilePicture}
+               alt="Profile"
+               style={styles.profileImage}
+             />
+           </div>
+           <div>
+             <label style={styles.label}>User Name</label>
+             {isEditing ? (
+               <input
+                 type="text"
+                 value={userInfo.userName}
+                 onChange={(e) => handleInputChange("userName", e.target.value)}
+               />
+             ) : (
+               <p style={styles.info}>{userInfo.userName}</p>
+             )}
+           </div>
+           <div>
+             <label style={styles.label}>Email</label>
+             <p style={styles.info}>{userInfo.email}</p>
+           </div>
+           <div>
+             <label style={styles.label}>Phone Number</label>
+             <p style={styles.info}>{userInfo.phoneNumber}</p>
+           </div>
+           <div>
+             <label style={styles.label}>Address</label>
+             <p style={styles.info}>{userInfo.address}</p>
+           </div>
+           <div style={styles.buttonGroup}>
+             {isEditing ? (
+               <button onClick={handleSaveClick} style={styles.saveButton}>
+                 Save
+               </button>
+             ) : (
+               <button onClick={handleEditClick} style={styles.editButton}>
+                 Edit
+               </button>
+             )}
+             <button onClick={handleDeleteClick} style={styles.deleteButton}>
+               Delete
+             </button>
+           </div>
+         </div>
+       </div>
+     );
    };
 
-   return (
-     <Container className="profile">
-       <h2>Your Profile</h2>
-       <Row>
-         <Col md={4}>
-           {dummyUser.profilePhoto && (
-             <div className="profile-photo">
-               <Image src={dummyUser.profilePhoto} alt="Profile" fluid />
-             </div>
-           )}
-         </Col>
-         <Col md={8}>
-           <p>
-             <strong>Name:</strong> {dummyUser.firstName} {dummyUser.lastName}
-           </p>
-           <p>
-             <strong>Email:</strong> {dummyUser.email}
-           </p>
-         </Col>
-       </Row>
-     </Container>
-   );
- };
+   const styles = {
+     container: {
+       maxWidth: "400px",
+       margin: "0 auto",
+       padding: "20px",
+       border: "1px solid #ccc",
+       borderRadius: "5px",
+       backgroundColor: "#f7f7f7",
+     },
+     heading: {
+       fontSize: "24px",
+       marginBottom: "20px",
+     },
+     userInfo: {
+       marginBottom: "15px",
+     },
+     label: {
+       marginBottom: "5px",
+       fontWeight: "bold",
+     },
+     profileImage: {
+       maxWidth: "150px",
+       borderRadius: "5px",
+     },
+     info: {
+       marginBottom: "10px",
+     },
+     buttonGroup: {
+       marginTop: "20px",
+     },
+     editButton: {
+       marginRight: "10px",
+       backgroundColor: "#007bff",
+       color: "#fff",
+       border: "none",
+       borderRadius: "5px",
+       padding: "5px 10px",
+       cursor: "pointer",
+     },
+     saveButton: {
+       marginRight: "10px",
+       backgroundColor: "#28a745",
+       color: "#fff",
+       border: "none",
+       borderRadius: "5px",
+       padding: "5px 10px",
+       cursor: "pointer",
+     },
+     deleteButton: {
+       backgroundColor: "#dc3545",
+       color: "#fff",
+       border: "none",
+       borderRadius: "5px",
+       padding: "5px 10px",
+       cursor: "pointer",
+     },
+   };
 
- export default Profile;
+   export default UserProfile;
